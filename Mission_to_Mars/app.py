aaac84158189c544371c159b3f1e584e40c70a45
Shadee Tabasi <shadeetabasi@gmail.com>
2021-02-25 18:19:35 -0500

    added flask file

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,14 @@
+#import necessary libraries and py file
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
