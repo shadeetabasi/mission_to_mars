dc947bf0f0b562dd925e1cfa5927a15d0a118f73
Shadee Tabasi <shadeetabasi@gmail.com>
2021-02-25 17:29:55 -0500

    finalized round 1 code on .py file

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -10,6 +10,7 @@ def init_browser():
 
 def scrape():
     browser = init_browser()
+    mars_complete_data = {}
 
     # URL of page to be scraped
     url = 'https://mars.nasa.gov/news/'
@@ -21,9 +22,82 @@ def scrape():
     html = browser.html
     soup = bs(html, "html.parser")
 
-    # scrape Mars latest news and recent title
-    titles = soup.find_all("div", {"class":'content_title'})
-    most_recent_title = titles[0]
+    ## 1. LATEST MARS NEWS: scrape Mars latest news and snippeets and isolate most recent title + paragraph
+    titles = soup.find_all("div", {"class":'content_title'}).text
+    most_recent_title = titles[0].text
+    mars_complete_data['nasa_news_title'] = most_recent_title
 
+    paragraphs = soup.find_all("div", {"class":'rollover_description_inner'}).text
+    most_recent_paragraph = paragraphs[0].text
+    mars_complete_data['nasa_news_snippet'] = most_recent_paragraph
 
+    ## 2. JPL MARS SPACE IMAGE : scrape featured Mars Image
+    mars_url = 'https://www.jpl.nasa.gov/images?query=mars'
+    browser.visit(mars_url)
+    #FIGURE OUT IF I NEED TO KEEP CHANGING html and soup name values
+    html = browser.html
+    soup = bs(html, 'html.parser')
 
+    #Find all images, save first image and display
+    mars_results = mars_soup.find_all('img', class_="BaseImage object-contain")
+    image_urls = [i['src'] for i in mars_results]
+    featured_image_url = image_urls[0]
+    mars_complete_data['featured_image'] = featured_image_url
+
+    ## 3. MARS FACTS: Collect Mars facts using Pandas
+    #Setting the URL to scrape
+    mars_facts_url = 'https://space-facts.com/mars/'
+    mars_facts_tables = pd.read_html(mars_facts_url)
+    mars_facts_df = mars_facts_tables[0]
+    updated_df = mars_facts_df.rename(columns={0: "Fact", 1: "Value"})
+    #Save df to html string
+    html_table = updated_df.to_html()
+    #Clean up table
+    final_html_table = html_table.replace('\n', '')
+    mars_complete_data['mars_facts_table'] = final_html_table
+
+    #4. HEMISPHERE DICTIONARY: 
+    hemispheres_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hemispheres_url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    # Set open lists
+    hemisphere_urls= soup.find_all('div', class_='item')
+    hemisphere_titles = []
+    img_urls = []
+    img_urls_list = []
+
+    #Run for loop to find titles and links to each individual hemisphere page
+    for x in hemisphere_urls:
+        #find h3 text
+        hem_title = x.find('h3').text
+        #append to list
+        hemisphere_titles.append(hem_title)
+        #find individual links to hemisphere pages
+        img_url = x.find('a')['href']
+        #append to list
+        img_urls.append(img_url)
+    #create full urls to visit    
+    img_urls_list = ['https://astrogeology.usgs.gov' + url for url in img_urls]
+
+    #Create open list for final jpg img urls in full size
+    final_jpg_urls = []
+
+    # For loop to pull out jpg urls
+    for i in img_urls_list:
+        #Visit and create html object for each link in img urls list
+        browser.visit(i)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        #search and find jpg links
+        jpg_bucket = soup.find('div', class_='downloads')
+        jpg_url = jpg_bucket.find('a')['href']
+        #append to list
+        final_jpg_urls.append(jpg_url)
+
+    #Create dictionary to hold titles and img/jpg urls
+    final_hemisphere_data = dict(zip(hemisphere_titles, final_jpg_urls))
+    mars_complete_data['mars_hemispheres'] = final_hemisphere_data
+
+    return mars_complete_data
