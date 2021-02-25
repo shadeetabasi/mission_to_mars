19698b326e147d85814175dc2d07f658e5031b3d
Shadee Tabasi <shadeetabasi@gmail.com>
2021-02-25 16:58:34 -0500

    updated ipynb and added code to py

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,6 +1,7 @@
 from splinter import Browser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import pandas as pd
+import time
 
 def init_browser():
     #Find a way to check this to ensure this is the right path???
@@ -10,7 +11,19 @@ def init_browser():
 def scrape():
     browser = init_browser()
 
-    # Begin webscraping
     # URL of page to be scraped
     url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    time.sleep(1)
+
+    #create html object and parse with bs
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    # scrape Mars latest news and recent title
+    titles = soup.find_all("div", {"class":'content_title'})
+    most_recent_title = titles[0]
+
+
 
