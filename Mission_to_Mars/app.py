6bc12eda04c45584b235a349ac59a2a1686f1341
Shadee Tabasi <shadeetabasi@gmail.com>
2021-02-25 19:02:07 -0500

    connect to mongo

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -6,8 +6,8 @@ import scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+# Use PyMongo to establish Mongo connection (make sure we are establishing connection to collection and not db)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_to_mars")
 
 
 if __name__ == "__main__":
